97135ec344ef568f43e1b67383dcda3a7741e97d
Hyeonwoo No <nhwgit@gmail.com>
2025-09-02 04:12:38 +0900

    Feat: SystemException을 처리하는 예외 핸들러 추가

--- user-service/src/main/java/com/pomingmatgo/userservice/global/exception/handler/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/pomingmatgo/userservice/global/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.pomingmatgo.userservice.global.exception.handler;
 
 import com.pomingmatgo.userservice.global.exception.BusinessException;
 import com.pomingmatgo.userservice.global.exception.ErrorCode;
+import com.pomingmatgo.userservice.global.exception.SystemException;
 import com.pomingmatgo.userservice.global.exception.dto.ErrorResponseDto;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +18,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ErrorResponseDto handleBusinessException(BusinessException e) {
+        return new ErrorResponseDto(e.getErrorCode());
+    }
+
+    @ExceptionHandler(SystemException.class)
+    public ErrorResponseDto handleBusinessException(SystemException e) {
         return new ErrorResponseDto(e.getErrorCode());
     }
 }
