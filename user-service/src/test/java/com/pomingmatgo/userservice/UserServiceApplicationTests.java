5cb95eaab3770868a1b24b8bfc55c505cd7acc76
nhwgit <nhwgit@gmail.com>
2025-09-01 18:24:46 +0900

    chore: user-service 추가

---
+++ user-service/src/test/java/com/pomingmatgo/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomingmatgo.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
