432fbedfca678bc7c0e87bbb3739f33807c2228f
nhwgit <nhwgit@gmail.com>
2025-09-01 20:05:59 +0900

    feat: Api 응답 클래스 추가

---
+++ user-service/src/main/java/com/pomingmatgo/userservice/global/ApiResponseDto.java
@@ -0,0 +1,19 @@
+package com.pomingmatgo.userservice.global;
+
+public class ApiResponseDto<T> {
+    private int status;
+    private String message;
+    private T data;
+
+    public ApiResponseDto(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ApiResponseDto(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+}
