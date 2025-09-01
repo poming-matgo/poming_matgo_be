1b2be71ccf90a63143633b9933033c455dbb6206
nhwgit <nhwgit@gmail.com>
2025-09-01 20:52:32 +0900

    feat: 회원가입 틀 구현(예외처리, 이메일 인증 해야함)

--- user-service/src/main/java/com/pomingmatgo/userservice/api/user/controller/RegisterController.java
+++ user-service/src/main/java/com/pomingmatgo/userservice/api/user/controller/RegisterController.java
@@ -5,44 +5,41 @@ import com.pomingmatgo.userservice.api.user.request.CheckNicknameRequest;
 import com.pomingmatgo.userservice.api.user.request.RegisterRequest;
 import com.pomingmatgo.userservice.domain.user.service.RegisterService;
 import com.pomingmatgo.userservice.global.ApiResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/register")
 public class RegisterController {
-    RegisterService registerService;
+    private final RegisterService registerService;
 
     //중복 이메일 체크
     @GetMapping("/check-email")
-    public ApiResponseDto<Void> checkEmailDuplicate(CheckEmailRequest req) {
+    public ApiResponseDto<Void> checkEmailDuplicate(@RequestParam @Valid CheckEmailRequest req) {
 
         boolean isDuplicate = registerService.isEmailDuplicate(req.getEmail());
         if(isDuplicate)
-            return new ApiResponseDto<Void>(HttpStatus.CONFLICT.value(), "이메일이 중복됩니다.");
+            return new ApiResponseDto<>(HttpStatus.CONFLICT.value(), "이메일이 중복됩니다.");
         else
-            return new ApiResponseDto<Void>(HttpStatus.OK.value(), "이메일이 중복되지 않습니다.");
+            return new ApiResponseDto<>(HttpStatus.OK.value(), "이메일이 중복되지 않습니다.");
     }
 
     //중복 닉네임 체크
     @GetMapping("/check-nickname")
-    public ApiResponseDto<Void> checkNicknameDuplicate(CheckNicknameRequest req) {
+    public ApiResponseDto<Void> checkNicknameDuplicate(@RequestParam  @Valid CheckNicknameRequest req) {
         boolean isDuplicate = registerService.isNicknameDuplicate(req.getNickName());
         if(isDuplicate)
-            return new ApiResponseDto<Void>(HttpStatus.CONFLICT.value(), "닉네임이 중복됩니다.");
+            return new ApiResponseDto<>(HttpStatus.CONFLICT.value(), "닉네임이 중복됩니다.");
         else
-            return new ApiResponseDto<Void>(HttpStatus.OK.value(), "닉네임이 중복되지 않습니다.");
+            return new ApiResponseDto<>(HttpStatus.OK.value(), "닉네임이 중복되지 않습니다.");
     }
 
     //회원가입 폼 제출
     @PostMapping
-    public void handlePostRegister(RegisterRequest req) {
+    public void handlePostRegister(@RequestBody @Valid RegisterRequest req) {
         registerService.register(req);
     }
 }
--- user-service/src/main/java/com/pomingmatgo/userservice/domain/user/repository/UserRepository.java
+++ user-service/src/main/java/com/pomingmatgo/userservice/domain/user/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
-    boolean existsByNickName(String nickname);
+    boolean existsByNickname(String nickname);
 }
