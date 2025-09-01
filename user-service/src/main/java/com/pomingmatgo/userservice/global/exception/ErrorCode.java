1a982eeee4486efb979c83ab33125d05a7dfadfd
Hyeonwoo No <nhwgit@gmail.com>
2025-09-02 03:07:43 +0900

    Fix: DUPLICATE_NICKNAME http status 변경

--- user-service/src/main/java/com/pomingmatgo/userservice/global/exception/ErrorCode.java
+++ user-service/src/main/java/com/pomingmatgo/userservice/global/exception/ErrorCode.java
@@ -7,7 +7,7 @@ public enum ErrorCode {
     //입력 값 에러
     INVALID_FORM_INPUT(400, "폼의 입력 데이터가 유효하지 않습니다."),
     DUPLICATE_EMAIL(407, "이메일이 중복됩니다."),
-    DUPLICATE_NICKNAME(400, "닉네임이 중복됩니다.");
+    DUPLICATE_NICKNAME(407, "닉네임이 중복됩니다.");
 
     private final int statusCode;
     private final String message;
