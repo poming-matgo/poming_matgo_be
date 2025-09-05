a34df2be979769cceb59dedd8dcda1e163007f87
Hyeonwoo No <nhwgit@gmail.com>
2025-09-05 21:10:31 +0900

    Style: 자체로그인시 json 필드 이름 수정

--- auth-service/src/main/java/com/pomingmatgo/authservice/api/request/LoginInfo.java
+++ auth-service/src/main/java/com/pomingmatgo/authservice/api/request/LoginInfo.java
@@ -1,13 +1,18 @@
 package com.pomingmatgo.authservice.api.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LoginInfo {
 
     private String email;
     private String password;
+
+    @JsonProperty("code_verifier")
     private String codeVerifier;
 }
