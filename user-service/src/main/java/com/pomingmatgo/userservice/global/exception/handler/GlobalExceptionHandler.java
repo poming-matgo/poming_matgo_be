5337bc63cba502dbd8b982d08712176ac37eba72
nhwgit <nhwgit@gmail.com>
2025-09-07 04:45:11 +0900

    Feat: 예외 발생시 로깅

--- user-service/src/main/java/com/pomingmatgo/userservice/global/exception/handler/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/pomingmatgo/userservice/global/exception/handler/GlobalExceptionHandler.java
@@ -4,12 +4,14 @@ import com.pomingmatgo.userservice.global.exception.BusinessException;
 import com.pomingmatgo.userservice.global.exception.ErrorCode;
 import com.pomingmatgo.userservice.global.exception.SystemException;
 import com.pomingmatgo.userservice.global.exception.dto.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BindException.class)
@@ -39,6 +41,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ErrorCode.SYSTEM_ERROR);
+        log.error("에러 발생: "+e.getClass() + ' ' + e.getMessage());
         return ResponseEntity
                 .status(errorResponse.getHttpStatus())
                 .body(errorResponse);
