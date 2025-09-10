00d72d19c8f7b1397bc9ae1227a2482cb584cd80
Hyeonwoo No <nhwgit@gmail.com>
2025-09-10 10:19:15 +0900

    Feat: 방 생성 로직 뼈대 구현

--- game-service/src/main/java/com/pomingmatgo/gameservice/api/handler/GameWebSocketHandler.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/api/handler/GameWebSocketHandler.java
@@ -5,6 +5,7 @@ import com.pomingmatgo.gameservice.domain.service.matgo.RoomService;
 import com.pomingmatgo.gameservice.domain.service.matgo.PrePlayService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
@@ -14,34 +15,35 @@ import reactor.core.publisher.Mono;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@RequiredArgsConstructor
 public class GameWebSocketHandler implements WebSocketHandler {
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final PrePlayService prePlayService = new PrePlayService();
-    private final RoomService roomService = new RoomService();
+    private final ObjectMapper objectMapper;
+    private final PrePlayService prePlayService;
+    private final RoomService roomService;
     private final ConcurrentHashMap<String, Long> userRoomMap = new ConcurrentHashMap<>();
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         return session.receive()
                 .flatMap(message -> handleMessage(message, session))
-                .onErrorResume(e -> {
-                    return Mono.empty(); //todo: error handling
-                })
+                .onErrorResume(e -> Mono.empty()) //todo: error handling
                 .then();
     }
 
-    private Mono<Void> handleMessage(WebSocketMessage message, WebSocketSession session) {
+    private Mono<Long> handleMessage(WebSocketMessage message, WebSocketSession session) {
         return Mono.fromCallable(() -> objectMapper.readValue(message.getPayloadAsText(), RequestEvent.class))
                 .flatMap(event -> handleEvent(event, session))
                 .onErrorResume(JsonProcessingException.class, e -> Mono.empty());
     }
 
-    private Mono<Void> handleEvent(RequestEvent event, WebSocketSession session) {
-        switch (event.getEventType().getType()) {
-            case "CREATE_ROOM":
-                long roomId = roomService.createRoom();
-                session.getAttributes().put("roomId", roomId);
-                userRoomMap.put(session.getId(), roomId);
-                return Mono.empty();
+    private Mono<Long> handleEvent(RequestEvent event, WebSocketSession session) {
+        switch (event.getEventType().getSubType()) {
+            case "JOIN_ROOM":
+                return roomService.joinRoom(event.getUserId())
+                        .flatMap(roomId -> {
+                            session.getAttributes().put("roomId", roomId);
+                            userRoomMap.put(session.getId(), roomId);
+                            return Mono.just(roomId);
+                        });
             case "CONNECT_USER":
                 prePlayService.connectUser(event.getUserId());
                 return Mono.empty();
--- game-service/src/main/java/com/pomingmatgo/gameservice/domain/service/matgo/RoomService.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/domain/service/matgo/RoomService.java
@@ -2,13 +2,20 @@ package com.pomingmatgo.gameservice.domain.service.matgo;
 
 import com.pomingmatgo.gameservice.domain.GameState;
 import com.pomingmatgo.gameservice.domain.repository.GameStateRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class RoomService {
-    GameStateRepository gameStateRepository;
-    public long createRoom() {
+    private final GameStateRepository gameStateRepository;
+    public Mono<Long> joinRoom(long userId) {
         GameState gameState = new GameState();
-        return gameStateRepository.save(gameState).getRoomId();
+        return gameStateRepository.save(gameState)
+                .map(roomId -> {
+                    return roomId;
+                });
     }
+
 }
