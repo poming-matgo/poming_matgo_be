c499308ee20776e136a8df000b21c289aa2aac4e
nhwgit <nhwgit@gmail.com>
2025-10-25 15:20:41 +0900

    Fix: 직렬화를 위해 ChoiceInfo에 lombok 생성자 추가

--- game-service/src/main/java/com/pomingmatgo/gameservice/domain/ChoiceInfo.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/domain/ChoiceInfo.java
@@ -1,14 +1,18 @@
 package com.pomingmatgo.gameservice.domain;
 
 import com.pomingmatgo.gameservice.domain.card.Card;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChoiceInfo implements Serializable {
     private int playerNumToChoose;
     private Card submittedCard;
