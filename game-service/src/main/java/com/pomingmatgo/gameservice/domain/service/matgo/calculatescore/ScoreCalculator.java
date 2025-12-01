002a37bd8d20b28329b1b93178bc31055a34853c
Hyeonwoo No <nhwgit@gmail.com>
2025-12-01 21:56:45 +0900

    Refactor: ScoreCalculator를 비동기코드에서 동기코드로 변경

--- game-service/src/main/java/com/pomingmatgo/gameservice/domain/service/matgo/calculatescore/ScoreCalculator.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/domain/service/matgo/calculatescore/ScoreCalculator.java
@@ -14,22 +14,19 @@ import java.util.stream.Stream;
 
 @Component
 public final class ScoreCalculator {
-    private ScoreCalculator() {}
-
-    public static Mono<Integer> calculatePiScore(List<Card> cards) {
+    public int calculatePiScore(List<Card> cards) {
         int piCnt = cards.stream().reduce(0, (acc, card) -> {
             if (!CardType.PI.equals(card.getType())) {
                 throw new IllegalArgumentException("피 카드가 아닙니다.");
             }
             return acc + (Objects.equals(card.getSpecialType(), SpecialType.SSANG_PI) ? 2 : 1);
         }, Integer::sum);
-        int result = piCnt < 10 ? 0 : piCnt - 9;
-        return Mono.just(result);
+        return piCnt < 10 ? 0 : piCnt - 9; // Mono.just() 제거
     }
 
-    public static Mono<Integer> calculateGwangScore(List<Card> cards) {
+    public int calculateGwangScore(List<Card> cards) {
         int size = cards.size();
-        int score = switch (size) {
+        return switch (size) { // 바로 return
             case 3 -> {
                 boolean hasBiGwang = cards.stream()
                         .anyMatch(card -> Objects.equals(card.getSpecialType(), SpecialType.BI_GWANG));
@@ -39,21 +36,19 @@ public final class ScoreCalculator {
             case 5 -> 15;
             default -> 0;
         };
-        return Mono.just(score);
     }
 
-    public static Mono<Integer> calculateKkutScore(List<Card> cards) {
+    public int calculateKkutScore(List<Card> cards) {
         int size = cards.size();
         boolean isGodori = cards.stream()
                 .filter(card -> card.getSpecialType() == SpecialType.GODORI)
                 .limit(3)
                 .count() >= 3;
         int godoriScore = isGodori ? 5 : 0;
-        int score = size < 5 ? godoriScore : godoriScore + size - 4;
-        return Mono.just(score);
+        return size < 5 ? godoriScore : godoriScore + size - 4; // Mono.just() 제거
     }
 
-    public static Mono<Integer> calculateDdiScore(List<Card> cards) {
+    public int calculateDdiScore(List<Card> cards) {
         long size = cards.size();
         Map<SpecialType, Long> specialTypeCount = cards.stream()
                 .filter(card -> card.getSpecialType() != null)
@@ -61,30 +56,20 @@ public final class ScoreCalculator {
         int additionalScore = (int) Stream.of(SpecialType.HONG_DAN, SpecialType.CHO_DAN, SpecialType.CHUNG_DAN)
                 .filter(type -> specialTypeCount.getOrDefault(type, 0L) >= 3)
                 .count() * 3;
-        int score = size < 5 ? additionalScore : additionalScore + (int) size - 4;
-        return Mono.just(score);
+        return size < 5 ? additionalScore : additionalScore + (int) size - 4; // Mono.just() 제거
     }
 
-    public static Mono<Integer> calculateTotalScore(List<Card> cards) {
-        List<Card> piCards = cards.stream()
-                .filter(card -> card.getType() == CardType.PI)
-                .toList();
-        List<Card> gwangCards = cards.stream()
-                .filter(card -> card.getType() == CardType.GWANG)
-                .toList();
-        List<Card> kkutCards = cards.stream()
-                .filter(card -> card.getType() == CardType.KKUT)
-                .toList();
-        List<Card> ddiCards = cards.stream()
-                .filter(card -> card.getType() == CardType.DDI)
-                .toList();
+    public int calculateTotalScore(List<Card> cards) {
+        List<Card> piCards = cards.stream().filter(c -> c.getType() == CardType.PI).toList();
+        List<Card> gwangCards = cards.stream().filter(c -> c.getType() == CardType.GWANG).toList();
+        List<Card> kkutCards = cards.stream().filter(c -> c.getType() == CardType.KKUT).toList();
+        List<Card> ddiCards = cards.stream().filter(c -> c.getType() == CardType.DDI).toList();
 
-        Mono<Integer> piScore = calculatePiScore(piCards);
-        Mono<Integer> gwangScore = calculateGwangScore(gwangCards);
-        Mono<Integer> kkutScore = calculateKkutScore(kkutCards);
-        Mono<Integer> ddiScore = calculateDdiScore(ddiCards);
+        int piScore = calculatePiScore(piCards);
+        int gwangScore = calculateGwangScore(gwangCards);
+        int kkutScore = calculateKkutScore(kkutCards);
+        int ddiScore = calculateDdiScore(ddiCards);
 
-        return Mono.zip(piScore, gwangScore, kkutScore, ddiScore)
-                .map(tuple -> tuple.getT1() + tuple.getT2() + tuple.getT3() + tuple.getT4());
+        return piScore + gwangScore + kkutScore + ddiScore;
     }
 }
