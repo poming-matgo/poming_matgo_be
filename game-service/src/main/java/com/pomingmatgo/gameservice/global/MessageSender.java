975ce34594b6492d01cadc074b7b05a52cc973d9
Hyeonwoo No <nhwgit@gmail.com>
2026-03-26 04:47:48 +0900

    Refactor: 불필요 코드들 정리

--- game-service/src/main/java/com/pomingmatgo/gameservice/global/MessageSender.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/global/MessageSender.java
@@ -4,13 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pomingmatgo.gameservice.global.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Sinks;
-
-import java.util.Collection;
 
 @Component
 @RequiredArgsConstructor
@@ -29,38 +25,9 @@ public class MessageSender {
                 .onErrorResume(e -> Mono.empty());
     }
 
-    public <T> Mono<Void> sendMessageToUsers(Collection<WebSocketSession> users, WebSocketResDto<T> response) {
-        return Flux.fromIterable(users)
-                .flatMap(session -> sendMessageToSession(session, response))
-                .then();
-    }
-
     public <T> Mono<Void> sendMessageToAllUser(long roomId, WebSocketResDto<T> response) {
         return Flux.fromIterable(sessionManager.getAllUser(roomId))
                 .flatMap(session -> sendMessageToSession(session, response))
                 .then();
     }
-
-    public Mono<String> askUser(long roomId, int playerNum, String question) {
-        WebSocketSession session = sessionManager.getSession(roomId, playerNum);
-        if (session == null) {
-            return Mono.error(new IllegalStateException("Session not found"));
-        }
-
-        Sinks.One<String> sink = Sinks.one();
-
-        session.send(Mono.just(session.textMessage(question)))
-                .doOnError(sink::tryEmitError)
-                .subscribe();
-
-        session.receive()
-                .map(WebSocketMessage::getPayloadAsText)
-                .next()
-                .doOnNext(sink::tryEmitValue)
-                .doOnError(sink::tryEmitError)
-                .subscribe();
-
-        return sink.asMono();
-    }
-
 }
