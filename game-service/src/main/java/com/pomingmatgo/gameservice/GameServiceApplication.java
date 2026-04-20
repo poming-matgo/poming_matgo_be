24179bf3a90464054c3dbf3a114df784e1fed51a
Hyeonwoo No <nhwgit@gmail.com>
2026-04-20 16:22:13 +0900

    Feat: BlockHound 추가

--- game-service/src/main/java/com/pomingmatgo/gameservice/GameServiceApplication.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/GameServiceApplication.java
@@ -2,13 +2,23 @@ package com.pomingmatgo.gameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GameServiceApplication {
 
     public static void main(String[] args) {
-        Hooks.onOperatorDebug();
+        /*Hooks.onOperatorDebug();
+        BlockHound.builder()
+                .allowBlockingCallsInside("org.redisson.command.CommandAsyncService", "get")
+                .allowBlockingCallsInside("org.redisson.connection.pool.ConnectionPool", "get")
+                .allowBlockingCallsInside("org.redisson.PubSubMessageListener", "onMessage")
+
+                .allowBlockingCallsInside("java.util.concurrent.ThreadPoolExecutor", "getTask")
+                .allowBlockingCallsInside("java.util.concurrent.LinkedBlockingQueue", "take")
+
+                .install();*/
         SpringApplication.run(GameServiceApplication.class, args);
     }
 
