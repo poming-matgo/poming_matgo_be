75a35dd5beb2a2ee7404254b254f2099fcbec274
Hyeonwoo No <nhwgit@gmail.com>
2026-07-07 05:52:02 +0900

    Refactor: RequestEvent 미사용 arrivalTime 필드 제거

--- game-service/src/main/java/com/pomingmatgo/gameservice/api/handler/event/RequestEvent.java
+++ game-service/src/main/java/com/pomingmatgo/gameservice/api/handler/event/RequestEvent.java
@@ -1,7 +1,6 @@
 package com.pomingmatgo.gameservice.api.handler.event;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +9,6 @@ import lombok.Setter;
 public class RequestEvent<T> {
     private EventType eventType;
     private T data;
-
-    @JsonIgnore
-    private long arrivalTime;
 
     @SuppressWarnings("unchecked")
     public <U> RequestEvent<U> as() {
